ddd6347ab153693149986c71da96e45b10d53695
Laxman Maharjan <lxmnmrzn@gmail.com>
2021-05-29 10:55:27 +0545

    Script to extension to dataset

---
+++ Adding_extensions.py
@@ -0,0 +1,13 @@
+import os
+
+def add_extension():
+    ''' Add extension to all midi file in the dataset directory
+
+    Args:
+        dataset_path: It is dataset path
+    '''
+    for file in os.listdir('.')
+
+if __name__ == '__main__':
+    
+    add_extension(path)
